3a691117575a41e9e57cb6bf2394f305c3d4172b
FreezingHeat <38347095+FreezingHeat@users.noreply.github.com>
2021-04-30 17:19:14 +0800

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,8 +1,4 @@
 module.exports = (req, res) => {
   // const date = new Date().toString();
-  res.status(200).json({
-      ips: req.ips,
-      ip: req.ip,
-      localAddress: req.socket.localAddress
-  })
+  res.status(200).json(req)
 };
