9da8c5374b4432b49145bc8a8c1c234eec7b26b0
FreezingHeat <38347095+FreezingHeat@users.noreply.github.com>
2021-04-30 17:43:21 +0800

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   // const date = new Date().toString();
-  req.status(200).json({
+  req.statusCode(200).json({
       ips: req.getHeader("X-FORWARDED-FOR") || req.getHeader("x-forwarded-for"),
       ip: req.getHeader("x-real-ip") || req.getHeader("X-REAL-IP") ,
       localAddress: req.localAddress
